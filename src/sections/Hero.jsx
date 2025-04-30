ec99e2ec04c90466bfd46289a9baa4ceaaffd554
Yacine Bensidahmed <158301080+Yacine4coding@users.noreply.github.com>
2025-04-30 08:21:06 +0100

    finish hero section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,66 @@
+
+import Button from '../components/Button'
+import HeroExperience from '../components/HeroModels/HeroExperience'
+import { words } from '../constants'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
+const Hero = () => {
+
+    
+
+  return (
+    <section id="hero" className="relative overflow-hidden">
+        <div className="absolute top-0 left-0 z-10">
+            <img src="/images/bg.png" alt="bg" />
+        </div>
+
+
+        <div className="hero-layout">
+            {/* LEFT: HERO CONTENT */}
+
+            <header className="flex flex-col justify-center md:w-full w-screen md:px-10 px-5">
+                <div className="flex flex-col gap-7">
+                    <div className="hero-text">
+                        <h1>
+                            Shaping
+                            <span className="slide">
+                                <span className="wrapper">
+                                    {words.map((word) =>(
+                                        <span key={word.text} className="flex items-center">
+                                            <img 
+                                                src={word.imgPath} 
+                                                alt={word.text} 
+                                                className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                                            />
+                                            <span>{word.text}</span>
+                                        </span>
+                                    ))}
+                                </span>
+                            </span>
+                        </h1>
+                        <h1>into Real Projects</h1>
+                        <h1>That Delivers Results</h1>
+                    </div>
+                    <p className='text-white-50 md:text-xl relative z-10 pointer-events-none'>Hi! I'm Yacine, a Computer Science Student and a Web Developer</p>
+                    <Button 
+                        className="md:w-80 md:h-16 w-60 h-12"
+                        id="button"
+                        text="See my Work"
+                    />
+                </div>
+            </header>
+
+            {/* RIGHT: 3D MODEL */}
+            <figure>
+                <div className="hero-3d-layout">
+                    <HeroExperience />
+                </div>
+            </figure>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Hero
