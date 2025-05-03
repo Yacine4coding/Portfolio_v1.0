796125dec5eabcd3101f8a01f152a518b1537d56
Yacine Bensidahmed <158301080+Yacine4coding@users.noreply.github.com>
2025-05-03 11:44:38 +0100

    finish Tech Stack

---
+++ src/components/Models/TechLogos/TechIcon.jsx
@@ -0,0 +1,37 @@
+// import { Environment, useGLTF, Float, OrbitControls } from "@react-three/drei";
+// import { Canvas } from "@react-three/fiber";
+// import React, { useEffect } from "react";
+// import { color } from "three/tsl";
+
+const TechIcon = ({ model }) => {
+//   const scene = useGLTF(model.modelPath);
+
+//   useEffect(() => {
+//     if(model.name === "Interactive Developer") {
+//       scene.scene.traverse((child) => {
+//         if (child.isMesh && child.name === "Object_5") {
+//           child.material = new THREE.MeshStandardMaterial({ color: "white" });
+//         }
+//       });
+//     }
+//   })
+
+  return (
+    // <Canvas>
+    //   <ambientLight intensity={0.3} />
+    //   <directionalLight intensity={1} position={[5, 5, 5]} />
+
+    //   <Environment preset="city" />
+
+    //   <OrbitControls enableZoom={false} />
+    //   <Float speed={5.5} rotationIntensity={0.5} floatIntensity={0.9}>
+    //     <group scale={model.scale} rotation={model.rotation}>
+    //       <primitive object={scene.scene} />
+    //     </group>
+    //   </Float>
+    // </Canvas>
+    <p>hello world!</p>
+  );
+};
+
+export default TechIcon;
---
+++ src/sections/TechStack.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import TitleHeader from "../components/TitleHeader";
+import { techStackIcons, techStackImgs } from "../constants";
+import TechIcon from "../components/Models/TechLogos/TechIcon";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const TechStackIcons_3D = () => (
+  <>
+    {techStackIcons.map((icon, index) => (
+      <div
+        key={icon.name}
+        className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
+      >
+        <div className="tech-card-animated-bg" />
+        <div className="tech-card-content">
+          <div className="tech-icon-wrapper">
+            <TechIcon model={icon} />
+          </div>
+
+          <div className="padding-x w-full">
+            <p>{icon.name}</p>
+          </div>
+        </div>
+      </div>
+    ))}
+  </>
+);
+
+const TechStackIcons_2D = () => (
+  <>
+    {techStackImgs.map((icon, index) => (
+      <div
+        key={icon.name}
+        className="card-border tech-card overflow-hidden group xl:rounded-full rounded-lg"
+      >
+        <div className="tech-card-animated-bg" />
+        <div className="tech-card-content">
+          <div className="tech-icon-wrapper">
+            <img src={icon.imgPath} alt={icon.name} />
+          </div>
+
+          <div className="padding-x w-full">
+            <p>{icon.name}</p>
+          </div>
+        </div>
+      </div>
+    ))}
+  </>
+);
+
+const TechStack = () => {
+  useGSAP(() => {
+    gsap.fromTo(
+      ".tech-card",
+      { opacity: 0 },
+      {
+        opacity: 1,
+        y: 0,
+        stagger: 0.2,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: "#skills",
+          start: "top center",
+        },
+      }
+    );
+  });
+
+  return (
+    <div id="skills" className="flex-center section-padding">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="My Preferred Tech Stack"
+          sub="🤝 The Skills I Bring to the Table"
+        />
+        <div className="tech-grid">
+          <TechStackIcons_2D />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TechStack;
