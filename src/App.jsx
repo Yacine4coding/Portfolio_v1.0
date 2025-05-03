db65706263d19bdaa04e4184bc6ade335af52341
Yacine Bensidahmed <158301080+Yacine4coding@users.noreply.github.com>
2025-05-03 18:33:11 +0100

    fix footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ExperienceSection from "./sections/ExperienceSection";
 import TechStack from "./sections/TechStack";
 import Testimonials from "./sections/Testimonials";
 import Contact from "./sections/Contact";
-import Footer from "./sections/footer";
+import Footer from "./sections/Footer";
 
 const App = () => {
   return (
