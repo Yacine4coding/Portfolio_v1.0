93a1fdfb3c01de54a2282a7702b9696cb1772b39
Yacine Bensidahmed <158301080+Yacine4coding@users.noreply.github.com>
2025-05-03 18:44:07 +0100

    testing : remove base from vite

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  base: '/Portfolio_v1.0',
   plugins: [
     react(),
     tailwindcss(),
