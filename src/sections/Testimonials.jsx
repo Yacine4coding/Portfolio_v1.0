ff8c8b410cd8ae64b1756ddcc3a6bba1ff22fdef
Yacine Bensidahmed <158301080+Yacine4coding@users.noreply.github.com>
2025-05-03 13:52:09 +0100

    finish testimonials

---
+++ src/sections/Testimonials.jsx
@@ -0,0 +1,33 @@
+import TitleHeader from "../components/TitleHeader";
+import { testimonials } from "../constants";
+import GlowCard from "../components/GlowCard";
+
+const Testimonials = () => {
+  return (
+    <section className="flex-center section-padding" id="testimonials">
+      <div className="w-full h-full md:px-10 px-5">
+        <TitleHeader
+          title="What People Say About Me?"
+          sub="⭐ Client Feedbacks Highlights"
+        />
+        <div className="lg:columns-3 md:columns-2 columns-1 mt-16">
+          {testimonials.map(({ imgPath, name, review, mentions }) => (
+            <GlowCard key={name} card={{ review }}>
+              <div className="flex items-center gap-3">
+                <div>
+                  <img src={imgPath} alt={name} />
+                </div>
+                <div className="flex flex-col">
+                  <p className="font-bold">{name}</p>
+                  <p className="text-white-50">{mentions}</p>
+                </div>
+              </div>
+            </GlowCard>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
