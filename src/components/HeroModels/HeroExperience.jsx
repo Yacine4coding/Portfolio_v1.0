d1fe9d11847887c543da0b501745d641d753373f
Yacine Bensidahmed <158301080+Yacine4coding@users.noreply.github.com>
2025-05-04 08:45:09 +0100

    remove particls + add with to 3d hero model

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -23,7 +23,7 @@ const HeroExperience = () => {
         />
 
         <HeroLights />
-        <Particles count={100} />
+        {/* <Particles count={100} /> */}
         <group
             scale={isMobile? 0.7 : 1}
             position={[0, -3.5, 0]}
